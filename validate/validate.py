64e1736466b53ccb91a56c3efce4e49d9720a5ba
Lucian DiPeso <ldipeso@uw.edu>
2019-04-16 12:10:52 -0700

    No longer scoring. Now presenting cell-by-cell FP,TP,FN,TN rates.

--- validate/validate.py
+++ validate/validate.py
@@ -4,7 +4,7 @@
 Calculates scores for a given classifier
 
 Usage:
-  validate.py CLASSIFIER [--test-data-folder=validate/validation-data/input/] [--classifier-conf=0] [--simple-output=0] [--max-processes=4]
+  validate.py CLASSIFIER [--test-data-folder=validate/validation-data/input/] [--classifier-conf=0] [--max-processes=4] [--skip-graphs=0] [--skip-filtered=0]
 
 Arguments:
   CLASSIFIER The name of the classifier to test
@@ -12,8 +12,9 @@ Arguments:
 Options:
   --test-data-folder=<string> [defaults: validate/validation-data/input] The directory with the CSV file containing particle data with true events
   --classifier-conf=<string> [defaults: None] Will be passed along to the classifier.
-  --simple-output=<int> [defaults: False] If simple, machine-readable output is desired.
   --max-processes=<int> [defaults: 4] The number of threads we will allow the classifier to use.
+  --skip-graphs=<bool> [defaults: False] If True, won't output graphs or videos
+  --skip-filtered=<bool> [defaults: False] If True, won't include filtered data in summary stats
 
 Output:
   Prints alignment scores, their mean, and their SD.
@@ -37,9 +38,7 @@ import csv
 import subprocess
 import math
 import re
-
-from Bio import pairwise2
-from Bio.pairwise2 import format_alignment
+from tabulate import tabulate
 from multiprocessing import Pool, cpu_count
 
 def colorize(color, string):
@@ -65,37 +64,40 @@ def colorize(color, string):
 
   return "\033[" + colors[color] + "m" + string + "\033[0m"
 
-def _print(silence, string):
-  if not silence:
-    print(string)
-
 ### Constant for getting our base input dir
 QA_PATH  = (ROOT_PATH / ("validate/qa.py")).resolve()
+
+STATS_EVENT_MAP = {
+  "R": "Rupture",
+  "M": "Mitosis",
+  "X": "Apoptosis"
+}
 
 ### Arguments and inputs
 arguments = docopt(__doc__, version=get_version())
 
-simple_output = True if arguments['--simple-output'] else False
-
 classifier = re.sub(r'[^a-zA-Z0-9\-\_\.\+]', '', arguments['CLASSIFIER'])
 if classifier != arguments['CLASSIFIER']:
-  _print(simple_output, colorize("yellow", "Classifier input has been sanitized to " + classifier))
+  print(colorize("yellow", "Classifier input has been sanitized to " + classifier))
 
 data_file_path = Path(arguments['--test-data-folder']).resolve() if arguments['--test-data-folder'] else Path("validate/validation-data/input/").resolve()
 if not data_file_path.exists():
-  _print(simple_output, colorize("red", "Data folder input cannot be found: \033[1m" + str(data_file_path) + "\033[0m"))
+  print(colorize("red", "Data folder input cannot be found: \033[1m" + str(data_file_path) + "\033[0m"))
   exit(1)
 
 classifier_conf = arguments['--classifier-conf'] if arguments['--classifier-conf'] else "0"
 
 max_processes = int(arguments['--max-processes']) if arguments['--max-processes'] else 4
 
+skip_graphs = bool(arguments['--skip-graphs']) if arguments['--skip-graphs'] else False
+skip_filtered = bool(arguments['--skip-filtered']) if arguments['--skip-filtered'] else False
+
 classifier_path = (ROOT_PATH / ("classifiers/" + classifier + ".py")).resolve()
 output_path     = (ROOT_PATH / ("validate/output/" + classifier)).resolve()
 output_path.mkdir(exist_ok=True)
 
 ### Run prediction
-_print(simple_output, "Running classifier \033[1m" + classifier + "\033[0m...")
+print("Running classifier \033[1m" + classifier + "\033[0m...")
 results_path = (output_path).resolve()
 cmd = [
   "python",
@@ -103,13 +105,11 @@ cmd = [
   str(data_file_path),
   str(results_path),
   "--conf=" + classifier_conf,
-  "--max-processes=" + str(max_processes)
+  "--max-processes=" + str(max_processes),
+  "--skip-graphs=" + str(skip_graphs)
 ]
 try:
-  if simple_output:
-    subprocess.check_call(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-  else:
-    subprocess.check_call(cmd)
+  subprocess.check_call(cmd)
 except:
   print(colorize("red", "Could not run classifier \033[1m" + classifier + "\033[0m"))
   exit(1)
@@ -117,98 +117,55 @@ except:
 ### Read our predicted data
 results_file_path = output_path / "results.csv"
 data = pd.read_csv(str(results_file_path), header=0, dtype={ 'particle_id': str })
-data = data[[ 'data_set', 'particle_id', 'time', 'filtered', 'event', 'true_event' ]]
 
+def get_cell_stats(p_data, skip_filtered):
+  data_set = p_data['data_set'].iloc[0]
+  particle_id = p_data['particle_id'].iloc[0]
 
-### Score our data
-# We will use sequence alignment algorithms to align a particles events with true_events
-# and score the alignment
+  result = pd.DataFrame({
+    'data_set': [ data_set ],
+    'particle_id': [ particle_id ]
+  })
+  
+  if skip_filtered and 'filtered' in p_data.columns:
+    p_data = p_data.loc[(p_data['filtered'] == 0)]
 
-def score_sequence(p_data): 
-  """
-  Score using sequence alignment algorithm
+  for event,name in STATS_EVENT_MAP.items():
+    result['pred' + event] = True if event in p_data['event'].unique() else False
+    result['true' + event] = True if event in p_data['true_event'].unique() else False
 
-  Arguments:
-    p_data Pandas DataFrame The data for a single particle
+  return result
 
-  Returns:
-    Alignment score
-  """
-  _print(simple_output, "  Scoring \033[1m" + p_data['data_set'].unique()[0] + ':' + p_data['particle_id'].unique()[0] + "\033[0m")
-  p_data = p_data.sort_values(by=[ 'time' ])
-  true = "".join(list(p_data['true_event']))
-  pred = "".join(list(p_data['event']))
+def get_summary_table(results):
+  names = []
+  true_positives = []
+  false_positives = []
+  true_negatives = []
+  false_negatives = []
+  for event,name in STATS_EVENT_MAP.items():
+    num_true_positive = results[((results['pred' + event] == True) & (results['true' + event] == True))].shape[0]
+    num_pred_positive = results[((results['pred' + event] == True))].shape[0]
+    num_true_negative = results[((results['pred' + event] == False) & (results['true' + event] == False))].shape[0]
+    num_pred_negative = results[((results['pred' + event] == False))].shape[0]
 
-  score_key = {
-    ('N', 'N'): 0.0001,
-    ('N', 'R'): -5,
-    ('N', 'E'): -5,
-    ('N', 'M'): -2,
-    ('N', 'X'): -2,
+    true_positive_rate = num_true_positive/num_pred_positive if num_pred_positive > 0 else 0
+    true_negative_rate = num_true_negative/num_pred_negative if num_pred_negative > 0 else 0
 
-    ('R', 'R'): 2,
-    ('R', 'N'): -1,
-    ('R', 'E'): 1.5,
-    ('R', 'M'): -2,
-    ('R', 'X'): -2,
+    names.append(name)
+    true_positives.append("{:.2%} ({}/{})".format(true_positive_rate, num_true_positive, num_pred_positive))
+    false_positives.append("{:.2%} ({}/{})".format((1-true_positive_rate), (num_pred_positive-num_true_positive), num_pred_positive))
+    true_negatives.append("{:.2%} ({}/{})".format(true_negative_rate, num_true_negative, num_pred_negative))
+    false_negatives.append("{:.2%} ({}/{})".format((1-true_negative_rate), (num_pred_negative-num_true_negative), num_pred_negative))
+  
+  summary = pd.DataFrame({
+    'event': names,
+    'true_positive': true_positives,
+    'false_positives': false_positives,
+    'true_negatives': true_negatives,
+    'false_negatives': false_negatives
+  })
 
-    ('E', 'E'): 2,
-    ('E', 'N'): -1,
-    ('E', 'R'): 1.5,
-    ('E', 'M'): -2,
-    ('E', 'X'): -2,
-
-    ('M', 'M'): 2,
-    ('M', 'N'): 0,
-    ('M', 'R'): -2,
-    ('M', 'E'): -2,
-    ('M', 'X'): -2,
-
-    ('X', 'X'): 2,
-    ('X', 'N'): 0,
-    ('X', 'R'): -2,
-    ('X', 'E'): -2,
-    ('X', 'M'): -2,
-
-    ('?', 'N'): -2,
-    ('?', 'R'): -1,
-    ('?', 'E'): -1,
-    ('?', 'M'): 0,
-    ('?', 'X'): 0
-  }
-  alignments = pairwise2.align.globalds(true, pred, score_key, -1, -0.5)
-  # perfect = pairwise2.align.globalds(true, true, score_key, -1, -0.5)
-  if len(alignments) >= 1:
-    return alignments[0][2]
-
-  return -89
-
-# def score_event(true, pred):
-#   """
-#   Score the true vs predicted event
-
-#   Gives very little weight to correctly predicting a non-event 
-#   frame. Also gives little weight if the true frame carries 
-#   death events, since we can't yet predict those.
-
-#   Arguments:
-#     true The true frame event
-#     pred The predicted frame event
-
-#   Returns:
-#     The score
-#   """
-#   if pred == 'N' and (true == 'X' or true == 'M'):
-#     return 0.01
-
-#   if true == pred:
-#     return 0.01 if true == 'N' else 5
-
-#   if true != 'N':
-#     # Penalize false positives
-#     return -8
-
-#   return -1
+  return summary
 
 def apply_parallel(grouped, fn, *args):
   """
@@ -229,11 +186,6 @@ def apply_parallel(grouped, fn, *args):
   with Pool(cpu_count()) as p:
     groups = []
     for name, group in grouped:
-      # We're going to skip sequences where true and pred
-      # values are entirely N
-      if (group['event'][( (group['event'] != 'N') & (group['filtered'] != 1) )].count() <= 0) and (group['event'][( (group['true_event'] != 'N') & (group['filtered'] != 1) )].count() <= 0):
-        _print(simple_output, "  Skipping \033[1m" + group['data_set'].unique()[0] + ':' + group['particle_id'].unique()[0] + "\033[0m")
-        continue
       t = tuple([ group ]) + tuple(args)
       groups.append(t)
     chunk = p.starmap(fn, groups)
@@ -241,16 +193,20 @@ def apply_parallel(grouped, fn, *args):
   return chunk
 
 if __name__ == '__main__':
-  _print(simple_output, "Scoring classifier \033[1m" + classifier + "\033[0m...")
-  test_results = apply_parallel(data.groupby([ 'data_set', 'particle_id' ]), score_sequence)
+  print("Scoring classifier \033[1m" + classifier + "\033[0m...")
+  results = apply_parallel(data.groupby([ 'data_set', 'particle_id' ]), get_cell_stats, skip_filtered)
+  results = pd.concat(results)
 
-_print(simple_output, "")
-if simple_output:
-  print(str(np.median(test_results)) + " " + str(np.std(test_results)))
-else:
-  print("\033[1mMedian:\033[0m {:.3f}".format(np.median(test_results)))
-  print("\033[1mStd. dev.:\033[0m {:.3f}".format(np.std(test_results)))
-_print(simple_output, "")
+  print("\033[1mTotal cells:\033[0m {}".format(results.shape[0]))
+  print()
+  print("All:")
+  print(tabulate(get_summary_table(results), headers=["Event", "True positives", "False positives", "True negatives", "False negatives"]))
+  print()
+  print("By dataset:")
+  for data_set in results['data_set'].unique():
+    print("\033[1m" + data_set + "\033[0m")
+    print(tabulate(get_summary_table(results[(results['data_set'] == data_set)]), headers=["Event", "True positives", "False positives", "True negatives", "False negatives"]))
+
 
 exit()
 
