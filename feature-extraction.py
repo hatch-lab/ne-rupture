865a356f3055c106ba00f35e2e0587b45037156d
Lucian DiPeso <ldipeso@uw.edu>
2019-06-05 12:20:07 -0700

    Now able to segment cells using Python, no Imaris required.

--- feature-extraction.py
+++ feature-extraction.py
@@ -38,11 +38,12 @@ from common.output import colorize
 import common.video as hatchvid
 
 import subprocess
-import numpy as np
-import cv2
-from skimage import measure, morphology, color, filters, segmentation, feature, util
-from scipy import ndimage as ndi
-from scipy import stats
+from multiprocessing import Pool, cpu_count
+
+import progressbar
+from time import sleep
+
+from feature_extraction import watershed
 
 ### Constants
 PREPROCESSOR_PATH  = (ROOT_PATH / ("fiji/frames-preprocessor.py")).resolve()
@@ -54,6 +55,8 @@ WIDTH              = 100
 arguments = docopt(__doc__, version=get_version())
 
 input_path = (ROOT_PATH / (arguments['INPUT'])).resolve()
+output_path = (input_path / ("input")).resolve()
+csv_path = output_path / "data-no-imaris.csv"
 raw_path = (input_path / ("images/raw")).resolve()
 processed_path = (input_path / (arguments['--img-dir'])) if arguments['--img-dir'] else (input_path / "images").resolve()
 data_set = arguments['--data-set'] if arguments['--data-set'] else (input_path).resolve().name
@@ -65,6 +68,47 @@ objective = int(arguments['--objective']) if arguments['--objective'] else 20
 microscope = arguments['--microscope'] if arguments['--microscope'] else "SD"
 pixel_size = arguments['--pixel-size'] if arguments['--pixel-size'] else 1
 rolling_ball_size = int(arguments['--rolling-ball-size']) if arguments['--pixel-size'] else 30
+
+def apply_parallel(frame_paths, m_frame_paths, fn, *args):
+  """
+  Function for parallelizing frame processing
+
+  Will take each raw/processed frame and pass 
+  that data to the provided function, along with the 
+  supplied arguments.
+
+  Arguments:
+    frame_paths list List of frame paths
+    m_frame_paths list List of processed frame paths
+    fn function The function called with a group as a parameter
+    args Arguments to pass through to fn
+
+  Returns:
+    Pandas DataFrame The re-assembled data.
+  """
+
+  with Pool(cpu_count()) as p:
+    groups = []
+    for i, frame_path in enumerate(frame_paths):
+      frame = cv2.imread(str(frame_path), cv2.IMREAD_GRAYSCALE)
+      m_frame = cv2.imread(str(m_frame_paths[i]), cv2.IMREAD_GRAYSCALE)
+      t = tuple([ i, frame, m_frame ]) + tuple(args)
+      groups.append(t)
+    rs = p.starmap_async(fn, groups)
+
+    widgets=[
+      ' [', progressbar.Timer(), '] ',
+      progressbar.AnimatedMarker()
+    ]
+
+    bar = progressbar.ProgressBar(widgets=widgets)
+    bar.start()
+
+    while not rs.ready():
+      bar.update()
+      sleep(0.1)
+
+  return pd.concat(rs.get(), sort=False)
 
 print("Processing TIFFs for mask-generation...")
 
@@ -96,177 +140,8 @@ frame_paths.sort()
 m_frame_paths = [ x for x in (processed_path / data_set).glob("*.tif") ]
 m_frame_paths.sort()
 
-# From https://github.com/jordan-g/Calcium-Imaging-Analysis/blob/master/imimposemin.py
-def imimposemin(I, BW, conn=None, max_value=255):
-  if not I.ndim in (2, 3):
-    raise Exception("'I' must be a 2-D or 3D array.")
+print("Extracting features...")
+cells = apply_parallel(frame_paths, m_frame_paths, watershed.process_frame)
+output_path.mkdir(mode=0o755, parents=True, exist_ok=True)
 
-  if BW.shape != I.shape:
-    raise Exception("'I' and 'BW' must have the same shape.")
-
-  if BW.dtype is not bool:
-    BW = BW != 0
-
-  # set default connectivity depending on whether the image is 2-D or 3-D
-  if conn == None:
-    if I.ndim == 3:
-      conn = 26
-    else:
-      conn = 8
-  else:
-    if conn in (4, 8) and I.ndim == 3:
-      raise Exception("'conn' is invalid for a 3-D image.")
-    elif conn in (6, 18, 26) and I.ndim == 2:
-      raise Exception("'conn' is invalid for a 2-D image.")
-
-  # create structuring element depending on connectivity
-  if conn == 4:
-    selem = morphology.disk(1)
-  elif conn == 8:
-    selem = morphology.square(3)
-  elif conn == 6:
-    selem = morphology.ball(1)
-  elif conn == 18:
-    selem = morphology.ball(1)
-    selem[:, 1, :] = 1
-    selem[:, :, 1] = 1
-    selem[1] = 1
-  elif conn == 26:
-    selem = morphology.cube(3)
-
-  fm = I.astype(float)
-
-  try:
-    fm[BW]                 = -math.inf
-    fm[np.logical_not(BW)] = math.inf
-  except:
-    fm[BW]                 = -float("inf")
-    fm[np.logical_not(BW)] = float("inf")
-
-  if I.dtype == float:
-    I_range = np.amax(I) - np.amin(I)
-
-    if I_range == 0:
-      h = 0.1
-    else:
-      h = I_range*0.001
-  else:
-    h = 1
-
-  fp1 = I + h
-
-  g = np.minimum(fp1, fm)
-
-  # perform reconstruction and get the image complement of the result
-  if I.dtype == float:
-    J = morphology.reconstruction(1 - fm, 1 - g, selem=selem)
-    J = 1 - J
-  else:
-    J = morphology.reconstruction(255 - fm, 255 - g, method='dilation', selem=selem)
-    J = 255 - J
-
-  try:
-    J[BW] = -math.inf
-  except:
-    J[BW] = -float("inf")
-
-  return J
-
-def segment_by_julien(m_frame):
-  threshold = filters.threshold_yen(m_frame)
-  th_frame = m_frame.copy()
-  th_frame[(th_frame < threshold)] = 0
-
-  # m_frame[(m_frame < threshold)] = 0
-  # th_frame = cv2.threshold(m_frame, threshold, 255, cv2.THRESH_BINARY)[1]
-
-  disk = morphology.disk(8)
-  frame_eroded = morphology.erosion(th_frame, disk)
-  frame_recon = morphology.reconstruction(seed=frame_eroded, mask=th_frame, method='dilation')
-  frame_dilated = morphology.dilation(frame_recon, disk)
-  frame_recon2 = morphology.reconstruction(seed=frame_dilated, mask=frame_recon, method='erosion')
-
-  # Foreground detection
-  kernel = np.zeros((3, 3))
-  kernel[0,1] = 1
-  kernel[1,] = 1
-  kernel[2,1] = 1
-  foreground = feature.peak_local_max(frame_recon2, footprint=kernel, indices=False, exclude_border=False).astype('uint8')
-  
-  disk = morphology.disk(1)
-  foreground = morphology.closing(foreground, disk)
-  foreground = morphology.erosion(foreground, disk)
-  # If we pass an array of ints, remove_small_objects thinks they are *labels* not *values*
-  morphology.remove_small_objects(foreground.astype('bool'), min_size=50, connectivity=2, in_place=True)
-  _, labeled_fg = cv2.connectedComponents(foreground.astype(np.uint8))
-
-  # Background detection
-  threshold = filters.threshold_yen(frame_recon2)
-  bin_frame = cv2.threshold(frame_recon2, threshold, 255, cv2.THRESH_BINARY)[1]
-  D = ndi.distance_transform_edt(bin_frame)
-  D_labels = segmentation.watershed(D)
-  background = (D_labels == 0).astype('uint8')
-
-  sobelx = cv2.Sobel(m_frame, cv2.CV_64F, 1, 0, ksize=3)
-  sobely = cv2.Sobel(m_frame, cv2.CV_64F, 0, 1, ksize=3)
-  gradient = np.sqrt(sobelx**2 + sobely**2)
-  gradient = 255*(gradient - np.min(gradient)) / (np.max(gradient) - np.min(gradient))
-  gradient = imimposemin(gradient, np.bitwise_or(background, foreground))
-
-  labels = segmentation.watershed(gradient, labeled_fg)
-
-  remove = stats.mode(labels)[0][0][0]
-  labels_2 = labels.copy()
-  segmentation.clear_border(labels_2, in_place=True)
-  labels_2[labels == remove] = 0
-
-  return labels_2
-
-def segment_by_lucian(m_frame, threshold):
-  m_frame[(m_frame < threshold)] = 0
-  th_frame = cv2.threshold(m_frame, threshold, 255, cv2.THRESH_BINARY)[1]
-
-  # m_frame_o = morphology.closing(m_frame, morphology.disk(8))
-  m_frame_o = morphology.opening(th_frame, morphology.disk(8))
-  # m_frame_o = morphology.erosion(m_frame_o, morphology.disk(8))
-
-  # m_frame_c = morphology.closing(m_frame_o, morphology.disk(8))
-  # m_frame_c = morphology.dilation(m_frame_c, morphology.disk(8))
-  # m_frame_c = morphology.reconstruction(util.invert(m_frame_c), util.invert(m_frame_o))
-
-  markers = filters.rank.gradient(m_frame_o, morphology.disk(10)) < 10
-  markers = ndi.label(markers)[0]
-  gradient = filters.rank.gradient(m_frame, morphology.disk(2))
-  labels = segmentation.watershed(gradient, markers)
-
-  return labels
-
-threshold = None
-for i,m_frame_path in enumerate(m_frame_paths):
-  frame = cv2.imread(str(frame_paths[i]), cv2.IMREAD_GRAYSCALE)
-  m_frame = cv2.imread(str(m_frame_path), cv2.IMREAD_GRAYSCALE)
-
-  if threshold is None:
-    threshold = filters.threshold_yen(m_frame)
-
-  j_labels = segment_by_julien(m_frame)
-  l_labels = segment_by_lucian(m_frame, threshold)
-  
-  label_image = measure.label(j_labels)
-  image_label_overlay_j = color.label2rgb(label_image, image=m_frame)
-  cv2.imshow('Julien', image_label_overlay_j)
-
-  label_image = measure.label(l_labels)
-  image_label_overlay_l = color.label2rgb(label_image, image=m_frame)
-  cv2.imshow('Lucian', image_label_overlay_l)
-
-  c = cv2.waitKey(0)
-  if 'q' == chr(c & 255):
-    cv2.destroyAllWindows() 
-    exit()
-
-cv2.destroyAllWindows()
-
-
-
-
+cells.to_csv(str(csv_path), header=True, encoding='utf-8', index=None)
---
+++ feature_extraction/watershed.py
@@ -0,0 +1,289 @@
+# coding=utf-8
+
+import sys
+import os
+from pathlib import Path
+
+ROOT_PATH = Path(__file__ + "/../..").resolve()
+
+sys.path.append(str(ROOT_PATH))
+
+from common.docopt import docopt
+
+import numpy as np
+np.seterr(divide='ignore', invalid='ignore') # Occasional divide-by-zero warnings with regionprops
+import pandas as pd
+import cv2
+from skimage import measure, morphology, color, filters, segmentation, feature, util
+from scipy import ndimage as ndi
+from scipy import stats
+
+# From https://github.com/jordan-g/Calcium-Imaging-Analysis/blob/master/imimposemin.py
+def imimposemin(I, BW, conn=None, max_value=255):
+  if not I.ndim in (2, 3):
+    raise Exception("'I' must be a 2-D or 3D array.")
+
+  if BW.shape != I.shape:
+    raise Exception("'I' and 'BW' must have the same shape.")
+
+  if BW.dtype is not bool:
+    BW = BW != 0
+
+  # set default connectivity depending on whether the image is 2-D or 3-D
+  if conn == None:
+    if I.ndim == 3:
+      conn = 26
+    else:
+      conn = 8
+  else:
+    if conn in (4, 8) and I.ndim == 3:
+      raise Exception("'conn' is invalid for a 3-D image.")
+    elif conn in (6, 18, 26) and I.ndim == 2:
+      raise Exception("'conn' is invalid for a 2-D image.")
+
+  # create structuring element depending on connectivity
+  if conn == 4:
+    selem = morphology.disk(1)
+  elif conn == 8:
+    selem = morphology.square(3)
+  elif conn == 6:
+    selem = morphology.ball(1)
+  elif conn == 18:
+    selem = morphology.ball(1)
+    selem[:, 1, :] = 1
+    selem[:, :, 1] = 1
+    selem[1] = 1
+  elif conn == 26:
+    selem = morphology.cube(3)
+
+  fm = I.astype(float)
+
+  try:
+    fm[BW]                 = -math.inf
+    fm[np.logical_not(BW)] = math.inf
+  except:
+    fm[BW]                 = -float("inf")
+    fm[np.logical_not(BW)] = float("inf")
+
+  if I.dtype == float:
+    I_range = np.amax(I) - np.amin(I)
+
+    if I_range == 0:
+      h = 0.1
+    else:
+      h = I_range*0.001
+  else:
+    h = 1
+
+  fp1 = I + h
+
+  g = np.minimum(fp1, fm)
+
+  # perform reconstruction and get the image complement of the result
+  if I.dtype == float:
+    J = morphology.reconstruction(1 - fm, 1 - g, selem=selem)
+    J = 1 - J
+  else:
+    J = morphology.reconstruction(255 - fm, 255 - g, method='dilation', selem=selem)
+    J = 255 - J
+
+  try:
+    J[BW] = -math.inf
+  except:
+    J[BW] = -float("inf")
+
+  return J
+
+def segment_by_julien(m_frame, vse=8, vse2=1, bwao=50):
+  Dfb = m_frame.copy()
+  # If we generate negative numbers, they'll be converted to 255!
+  Dfb[Dfb < 5] = 5 
+  Dfb = Dfb - 5
+
+  gradmag = filters.sobel(Dfb)
+  gradmag = gradmag / np.max(gradmag)
+
+  # Define markers by image opening and reconstruction
+  se = morphology.disk(vse)
+  # MatLab constructs disks a little differently; let's match that
+  se = se[1:-1,1:-1]
+  Jo = morphology.opening(Dfb, se)
+  Je = morphology.erosion(Jo, se)
+  Jor = morphology.reconstruction(seed=Je, mask=Dfb, method="dilation").astype(np.uint8)
+
+  # Foreground detection
+  # This is a little different from MATLAB
+  fgm = feature.peak_local_max(Jor, footprint=morphology.disk(30), indices=False, exclude_border=False).astype(np.uint8)
+  # fgm = morphology.erosion(fgm, morphology.disk(1))
+  # fgm = morphology.remove_small_objects(fgm.astype('bool'), min_size=bwao, connectivity=2).astype(np.uint8)
+  
+  se2 = morphology.disk(vse2)
+  fgm2 = morphology.closing(fgm, se2)
+  fgm3 = morphology.erosion(fgm2, se2)
+  # If we pass an array of ints, remove_small_objects thinks they are *labels* not *values*
+  fgm4 = morphology.remove_small_objects(fgm3.astype('bool'), min_size=bwao, connectivity=2).astype(np.uint8)
+
+  # Local background delimitation
+  threshold = filters.threshold_otsu(Jor)
+  bw = cv2.threshold(Jor, threshold, 1, cv2.THRESH_BINARY)[1]
+  D = ndi.distance_transform_edt(1-bw)
+  DL = segmentation.watershed(D, connectivity=2, watershed_line=True)
+
+  bgm = (DL == 0).astype('uint8')
+
+  gradmag2 = imimposemin(gradmag, np.bitwise_or(bgm, fgm4))-0.001
+
+  L = segmentation.watershed(gradmag2, connectivity=2, watershed_line=True)
+
+  rem = stats.mode(L)[0][0][0]
+  Ll = L.copy()
+  segmentation.clear_border(Ll, in_place=True)
+  Ll[L == rem] = 0
+
+  Ll = morphology.dilation(Ll, morphology.disk(2))
+
+  return Ll
+
+def segment_by_lucian(m_frame, threshold):
+  m_frame[(m_frame < threshold)] = 0
+  th_frame = cv2.threshold(m_frame, threshold, 255, cv2.THRESH_BINARY)[1]
+
+  # m_frame_o = morphology.closing(m_frame, morphology.disk(8))
+  m_frame_o = morphology.opening(th_frame, morphology.disk(8))
+  # m_frame_o = morphology.erosion(m_frame_o, morphology.disk(8))
+
+  # m_frame_c = morphology.closing(m_frame_o, morphology.disk(8))
+  # m_frame_c = morphology.dilation(m_frame_c, morphology.disk(8))
+  # m_frame_c = morphology.reconstruction(util.invert(m_frame_c), util.invert(m_frame_o))
+
+  markers = filters.rank.gradient(m_frame_o, morphology.disk(10)) < 10
+  markers = ndi.label(markers)[0]
+  gradient = filters.rank.gradient(m_frame, morphology.disk(2))
+  labels = segmentation.watershed(gradient, markers)
+
+  return labels
+
+def add_cell_props(cells, extra_info, props):
+  cells['data_set'].append(extra_info['data_set'])
+  cells['particle_id'].append(props.label)
+  cells['image_type'].append(extra_info['image_type'])
+  cells['mask'].append(extra_info['mask'])
+  cells['frame'].append(extra_info['frame'])
+
+  # Coordinates are row-column, not x-y
+  cells['x'].append(props.centroid[1]*extra_info['pixel_size']) 
+  cells['y'].append(props.centroid[0]*extra_info['pixel_size'])
+  cells['weighted_x'].append(props.weighted_centroid[1]*extra_info['pixel_size'])
+  cells['weighted_y'].append(props.weighted_centroid[0]*extra_info['pixel_size'])
+  
+  cells['area'].append(props.area*extra_info['pixel_size'])
+  cells['mean'].append(props.mean_intensity)
+  cells['max'].append(props.max_intensity)
+  cells['min'].append(props.min_intensity)
+  
+  cells['ellipse_eccentricity'].append(props.eccentricity)
+  cells['circle_diameter'].append(props.equivalent_diameter)
+  cells['ellipse_major_length'].append(props.major_axis_length)
+  cells['ellipse_minor_length'].append(props.minor_axis_length)
+  
+  cells['central_moment_11'].append(props.moments_central[0][0])
+  cells['central_moment_12'].append(props.moments_central[0][1])
+  cells['central_moment_13'].append(props.moments_central[0][2])
+  cells['central_moment_21'].append(props.moments_central[1][0])
+  cells['central_moment_22'].append(props.moments_central[1][1])
+  cells['central_moment_23'].append(props.moments_central[1][2])
+  cells['central_moment_31'].append(props.moments_central[2][0])
+  cells['central_moment_32'].append(props.moments_central[2][1])
+  cells['central_moment_33'].append(props.moments_central[2][2])
+
+  cells['perimeter'].append(props.perimeter)
+  cells['solidity'].append(props.solidity)
+
+  return cells
+
+def process_frame(frame_num, frame, m_frame):
+  cells = {
+    'data_set': [],
+    'particle_id': [],
+    'image_type': [],
+    'mask': [],
+    'frame': [],
+    'x': [],
+    'y': [],
+    'weighted_x': [],
+    'weighted_y': [],
+    'area': [],
+    'mean': [],
+    'max': [],
+    'min': [],
+    'ellipse_eccentricity': [],
+    'circle_diameter': [],
+    'ellipse_major_length': [],
+    'ellipse_minor_length': [],
+    'central_moment_11': [],
+    'central_moment_12': [],
+    'central_moment_13': [],
+    'central_moment_21': [],
+    'central_moment_22': [],
+    'central_moment_23': [],
+    'central_moment_31': [],
+    'central_moment_32': [],
+    'central_moment_33': [],
+    'perimeter': [],
+    'solidity': []
+  }
+
+  # Raw labelling
+  nuc_labels = segment_by_julien(m_frame)
+
+  # When nuclei are closer than the dilation, one will win
+  cyto_labels = morphology.dilation(nuc_labels, morphology.disk(5))
+  cyto_labels[nuc_labels > 0] = 0
+
+  nuc_raw_regions = measure.regionprops(nuc_labels, intensity_image=frame, coordinates='rc')
+  cyto_raw_regions = measure.regionprops(cyto_labels, intensity_image=frame, coordinates='rc')
+
+  nuc_processed_regions = measure.regionprops(nuc_labels, intensity_image=m_frame, coordinates='rc')
+  cyto_processed_regions = measure.regionprops(cyto_labels, intensity_image=m_frame, coordinates='rc')
+
+  for region in nuc_raw_regions:
+    info = {
+      'data_set': data_set,
+      'frame': frame_num,
+      'mask': 'nucleus',
+      'image_type': 'raw',
+      'pixel_size': pixel_size
+    }
+    cells = add_cell_props(cells, info, region)
+
+  for region in nuc_processed_regions:
+    info = {
+      'data_set': data_set,
+      'frame': frame_num,
+      'mask': 'nucleus',
+      'image_type': 'processed',
+      'pixel_size': pixel_size
+    }
+    cells = add_cell_props(cells, info, region)
+
+  for region in cyto_raw_regions:
+    info = {
+      'data_set': data_set,
+      'frame': frame_num,
+      'mask': 'cytoplasm',
+      'image_type': 'raw',
+      'pixel_size': pixel_size
+    }
+    cells = add_cell_props(cells, info, region)
+    
+  for region in cyto_processed_regions:
+    info = {
+      'data_set': data_set,
+      'frame': frame_num,
+      'mask': 'cytoplasm',
+      'image_type': 'processed',
+      'pixel_size': pixel_size
+    }
+    cells = add_cell_props(cells, info, region)
+
+  return pd.DataFrame(cells)
