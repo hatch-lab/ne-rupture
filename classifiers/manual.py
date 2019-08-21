be44e7ecdf2d0932cd6da4755ce0f638d7a0795f
Lucian DiPeso <ldipeso@uw.edu>
2019-08-21 11:10:56 -0700

    Improved UI, keyboard controls. Bug fixes in Manual classifier.

--- classifiers/manual.py
+++ classifiers/manual.py
@@ -63,16 +63,133 @@ def seed_events(data, tiff_path, conf):
     pd.DataFrame The modified particle data
   """
 
-  data = data.groupby([ 'data_set', 'particle_id' ]).apply(load_ui, Path(tiff_path).resolve(), conf)
-  exit()
+  title_font = ImageFont.truetype(str(FONT_PATH), size=15)
+  small_font = ImageFont.truetype(str(FONT_PATH), size=10)
+  p_event_labels = {
+    'N': '',
+    'R': 'Rupture',
+    'M': 'Mitosis',
+    'X': 'Apoptosis'
+  }
+
+  data_sets = data['data_set'].unique()
+  data_set_i = 0
+
+  while(data_set_i < len(data_sets)):
+    if data_set_i < 0:
+      # Break out of this data set and back track to previous data set
+      data_set_i = 0
+      continue
+
+    data_set = data_sets[data_set_i]
+    particle_ids = data.loc[( data['data_set'] == data_set ), 'particle_id'].unique()
+
+    p_id_i = 0
+
+    while(p_id_i < len(particle_ids)):
+      if p_id_i < 0:
+        # Break out of this data set and back track to previous data set
+        data_set_i -= 1
+        break
+
+      p_id = particle_ids[p_id_i]
+
+      p_data = data.loc[( (data['data_set'] == data_set) & ( data['particle_id'] == p_id ) )]
+
+      p_data.sort_values('frame')
+
+      start_frame_i = np.min(p_data['frame'])
+      end_frame_i = np.max(p_data['frame'])
+      frame_i = start_frame_i
+
+      while(frame_i <= end_frame_i):
+        coords_filter = (p_data['frame'] == frame_i)
+
+        coords = p_data[coords_filter]
+
+        if len(coords.index) <= 0: # We're missing a frame
+          frame_i += 1
+          continue
+
+        current_event = coords['event'].iloc[0]
+
+        frame_file_name = str(frame_i).zfill(4) + '.tif'
+        frame_path = (tiff_path / (data_set + "/" + frame_file_name)).resolve()
+
+        if not frame_path.exists(): # We're missing a frame
+          frame_i += 1
+          continue
+
+        raw_frame = cv2.imread(str(frame_path), cv2.IMREAD_GRAYSCALE)
+
+        x = int(round(coords['x'].iloc[0]/coords['x_conversion'].iloc[0]))
+        y = int(round(coords['y'].iloc[0]/coords['y_conversion'].iloc[0]))
+
+        # Crop down to just this particle
+        frame = hatchvid.crop_frame(raw_frame, x, y, conf['movie_width'], conf['movie_height'])
+        frame = cv2.resize(frame, None, fx=conf['movie_scale_factor'], fy=conf['movie_scale_factor'], interpolation=cv2.INTER_CUBIC)
+
+        # Add a space for text
+        frame = np.concatenate((np.zeros(( 40, frame.shape[1]), dtype=frame.dtype), frame), axis=0)
+
+        # Make frame text
+        title = p_id
+
+        # Describe the current event state
+        title += " " + p_event_labels[current_event]
+
+        hours = math.floor(coords['time'].iloc[0] / 3600)
+        minutes = math.floor((coords['time'].iloc[0] - (hours*3600)) / 60)
+        seconds = math.floor((coords['time'].iloc[0] - (hours*3600)) % 60)
+
+        label = "{:02d}h{:02d}'{:02d}\" ({:d})".format(hours, minutes, seconds, frame_i)
+
+        # Now add text
+        img = Image.fromarray(frame)
+        draw = ImageDraw.Draw(img)
+
+        controls = "[<-] Prev [->] Next [S] Skip \n[R] Rupture [M] Mitosis [X] Apoptosis [N] None"
+
+        draw.text((10, 10), title, fill='rgb(255,255,255)', font=title_font)
+        draw.text((10, 30), label, fill='rgb(255,255,255)', font=small_font)
+        draw.text((10, frame.shape[0]-40), controls, fill='rgb(255,255,255)', font=small_font)
+
+        # Get it back into OpenCV format
+        frame = np.array(img)
+
+        cv2.imshow('Cell', frame)
+        c = cv2.waitKey(0)
+        if c == 2:
+          frame_i -= 1
+          if frame_i < start_frame_i:
+            # End looping through frames, backtrack to previous particle
+            p_id_i -= 1
+            break 
+
+        elif c == 3:
+          frame_i += 1
+
+        elif chr(c & 255) == 's':
+          # Just skip the rest of this cell
+          p_id_i += 1
+          break
+
+        elif chr(c & 255) in [ 'r', 'm', 'x', 'n' ]:
+          data_idx = ( (data['frame'] == frame_i ) & (data['particle_id'] == p_id) & (data['data_set'] == data_set) )
+          data.loc[data_idx, 'event'] = chr(c & 255).upper()
+          p_data = data.loc[( (data['data_set'] == data_set) & ( data['particle_id'] == p_id ) )]
+
+        if 'q' == chr(c & 255):
+          exit()
+
+    data_set_i += 1
+
+  cv2.destroyAllWindows()
 
   return data
 
 def load_ui(p_data, tiff_path, conf):
-  title_font = ImageFont.truetype(str(FONT_PATH), size=15)
-  small_font = ImageFont.truetype(str(FONT_PATH), size=10)
-  data_set = p_data['data_set'].iloc[0]
-  p_id = p_data['particle_id'].iloc[0]
+  
 
   p_data.sort_values('frame')
 
@@ -87,10 +204,13 @@ def load_ui(p_data, tiff_path, conf):
     'X': 'Apoptosis'
   }
 
-
   while(this_frame_i <= end_frame_i):
-    coords_filter = p_data['frame'] == this_frame_i
+    coords_filter = (p_data['frame'] == this_frame_i)
     coords = p_data[coords_filter]
+
+    if len(coords.index) <= 0: # We're missing a frame
+      this_frame_i += 1
+      continue
 
     data_set = coords['data_set'].iloc[0]
     pid = coords['particle_id'].iloc[0]
@@ -99,7 +219,8 @@ def load_ui(p_data, tiff_path, conf):
     frame_file_name = str(this_frame_i).zfill(4) + '.tif'
     frame_path = (tiff_path / (data_set + "/" + frame_file_name)).resolve()
 
-    if coords['frame'].count() <= 0 or not frame_path.exists(): # We're missing a frame
+    if not frame_path.exists(): # We're missing a frame
+      this_frame_i += 1
       continue
 
     raw_frame = cv2.imread(str(frame_path), cv2.IMREAD_GRAYSCALE)
@@ -116,14 +237,14 @@ def load_ui(p_data, tiff_path, conf):
 
     # Make frame text
     title = pid
+    # Describe the current event state
+    title += " " + p_event_labels[current_event]
+
     hours = math.floor(coords['time'].iloc[0] / 3600)
     minutes = math.floor((coords['time'].iloc[0] - (hours*3600)) / 60)
     seconds = math.floor((coords['time'].iloc[0] - (hours*3600)) % 60)
 
     label = "{:02d}h{:02d}'{:02d}\" ({:d})".format(hours, minutes, seconds, this_frame_i)
-    
-    # Describe the current event state
-    label += " " + p_event_labels[current_event]
 
     # Now add text
     img = Image.fromarray(frame)
@@ -153,6 +274,8 @@ def load_ui(p_data, tiff_path, conf):
 
     if 'q' == chr(c & 255):
       exit()
+
+  cv2.destroyAllWindows()
 
   return p_data
 
