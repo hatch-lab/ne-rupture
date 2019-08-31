960d76847a569d3eb7a440e33cc09ac550ea673b
Lucian DiPeso <ldipeso@uw.edu>
2019-08-30 17:40:32 -0700

    Adding in changes to preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -4,30 +4,27 @@
 Gets data into a format readable by classifiers
 
 Usage:
-  preprocess.py PROCESSOR INPUT [--output-dir=0] [--img-dir=0] [--mip-dir=0] [--output-name=data.csv] [--data-dir=0] [--frame-rate=180] [--filter-window=5.0] [--gamma=0.50] [--channel=1] [--objective=20] [--microscope=SD] [--data-set=0] [--pixel-size=0] [--rolling-ball-size=30] [--keep-imgs=0]
+  preprocess.py PROCESSOR INPUT [--output-dir=<string>] [--img-dir=<string>] [--mip-dir=<string>] [--output-name=<string>] [--data-dir=<string>] [--frame-rate=<int>] [--filter-window=<int>] [--gamma=<float>] [--channel=<int>] [--data-set=<string>] [--pixel-size=<int>] [--keep-imgs] [--rolling-ball-size=<int>]
 
 Arguments:
   PROCESSOR The kind of image processor to use (eg, imaris or matlab)
   INPUT Path to the directory containing the raw data (CSV files for Imaris, TIFFs for MATLAB)
-  OUTPUT Path to where the classified data CSV file should be saved
 
 Options:
   -h --help Show this screen.
-  --output-dir=<string> [defaults: INPUT/output] The directory to save the resulting CSV file
-  --output-name=<string> [defaults: data.csv] The name of the resulting CSV file
-  --img-dir=<string> [defaults: INPUT/images/data_set] The path to TIFF files
-  --mip-dir=<string> [defaults: INPUT/images/data_set/mip] The path to MIP files
-  --data-dir=<string> [defaults: INPUT/images/raw for MATLAB; INPUT/input for Imaris]
-  --filter-window=<float> [defaults: 5.0] The window size used for the median pass filter, in px
-  --gamma=<float> [defaults: 0.50] The gamma correction to use
-  --channel=<int> [defaults: 1] The channel to keep (ie, the NLS-3xGFP channel)
-  --objective=<int> [defaults: 20] The microscope objective (eg, 20 for 20x)
-  --microscope=<string> [defaults: SD] "SPE" or "SD"
-  --data-set=<string|falsey> [defaults: None] The unique identifier for this data set. If none is supplied, the base file name of each TIFF will be used.
-  --pixel-size=<int|0> [defaults: 0] Specifying microscope and objective will automatically determine pixel size. If supplied here, that value will be used instead.
-  --rolling-ball-size=<int> [defaults: 30] The rolling ball diameter to use for rolling ball subtraction, in um
-  --frame-rate=<int> [defaults: 180] The seconds that elapse between frames
-  --keep-imgs=<int> [defaults: 0] Whether to store an image of each particle for each frame
+  --output-dir=<string>  [default: output] The subdirectory to save the resulting CSV file
+  --output-name=<string>  [default: data.csv] The name of the resulting CSV file
+  --img-dir=<string>  [defaults: INPUT/images/(data_set)] The path to TIFF files
+  --mip-dir=<string>  [defaults: INPUT/images/(data_set)/mip] The path to MIP files
+  --data-dir=<string>  Where to find the raw data. Typically determined by the preprocessor you've selected.
+  --filter-window=<int>  [default: 8] The window size used for the median pass filter, in px
+  --gamma=<float>  [default: 0.50] The gamma correction to use
+  --channel=<int>  [default: 1] The channel to keep (ie, the NLS-3xGFP channel)
+  --data-set=<string>  The unique identifier for this data set. If none is supplied, the base file name of each TIFF will be used.
+  --pixel-size=<int>  [default: 1] Pixels per micron. If 0, will attempt to detect automatically.
+  --rolling-ball-size=<int>  [default: 100] The rolling ball diameter to use for rolling ball subtraction, in um
+  --frame-rate=<int>  [default: 180] The seconds that elapse between frames
+  --keep-imgs  Whether to store an image of each particle for each frame
 
 Output:
   A CSV file with processed data
@@ -48,34 +45,53 @@ from common.output import colorize
 
 import re
 
-arguments = docopt(__doc__, version=get_version())
-### Arguments and inputs
-processor_name = re.sub(r'[^a-zA-Z0-9\-\_\.\+]', '', arguments['PROCESSOR'])
-if processor_name != arguments['PROCESSOR']:
-  print(colorize("yellow", "Classifier input has been sanitized to " + processor_name))
+from schema import Schema, And, Or, Use, SchemaError, Optional, Regex
 
-if not (ROOT_PATH / ("preprocessors/" + processor_name + ".py")).exists():
-  print(colorize("red", "No such processor exists"))
+arguments = docopt(__doc__, version=get_version())
+
+schema = Schema({
+  'PROCESSOR': And(len, lambda n: (ROOT_PATH / ('preprocessors/' + str(n) + '.py')).is_file(), error='That preprocessor does not exist'),
+  'INPUT': And(len, lambda n: os.path.exists(n), error='INPUT does not exist'),
+  '--output-dir': len,
+  '--output-name': len,
+  '--img-dir': Or(None, len),
+  '--mip-dir': Or(None, len),
+  '--data-dir': Or(None, len),
+  '--filter-window': And(Use(int), lambda n: n > 0, error='--filter-window must be > 0'),
+  '--gamma': And(Use(float), lambda n: n > 0, error='--gamma must be > 0'),
+  '--channel': And(Use(int), lambda n: n > 0),
+  '--data-set': Or(None, len),
+  '--pixel-size': And(Use(int), lambda n: n >= 0, error='--pixel-size must be > 0'),
+  '--rolling-ball-size': And(Use(int), lambda n: n > 0),
+  '--frame-rate': And(Use(int), lambda n: n > 0),
+  Optional('--keep-imgs'): bool
+})
+
+try:
+  arguments = schema.validate(arguments)
+except SchemaError as error:
+  print(error)
   exit(1)
 
-processor = import_module("preprocessors." + processor_name)
+### Arguments and inputs
+processor = import_module("preprocessors." + arguments['PROCESSOR'])
 
 input_path = (ROOT_PATH / (arguments['INPUT'])).resolve()
-output_path = (ROOT_PATH / (arguments['--output-dir'])).resolve() if arguments['--output-dir'] else (input_path / "input")
-output_name = arguments['--output-name'] if arguments['--output-name'] else "data.csv"
+output_path = (input_path / (arguments['--output-dir']))
+output_name = arguments['--output-name']
 data_path = (ROOT_PATH / (arguments['--data-dir'])).resolve() if arguments['--data-dir'] else processor.get_default_data_path(input_path)
 
-data_set = arguments['--data-set'] if arguments['--data-set'] else (input_path).resolve().name
-frame_rate = int(arguments['--frame-rate']) if arguments['--frame-rate'] else 180
+data_set = arguments['--data-set'] if arguments['--data-set'] else (input_path).name
+frame_rate = arguments['--frame-rate']
 
-tiff_path = (ROOT_PATH / (arguments['--img-dir'])).resolve() if arguments['--img-dir'] else (input_path / ("images/" + data_set))
-mip_path = (ROOT_PATH / (arguments['--mip-dir'])).resolve() if arguments['--mip-dir'] else (tiff_path / "mip")
+tiff_path = (input_path / (arguments['--img-dir'])).resolve() if arguments['--img-dir'] else (input_path / ("images/" + data_set))
+mip_path = (input_path / (arguments['--mip-dir'])).resolve() if arguments['--mip-dir'] else (tiff_path / "mip")
 
-filter_window = float(arguments['--filter-window']) if arguments['--filter-window'] else 5.0
-gamma = float(arguments['--gamma']) if arguments['--gamma'] else 0.50
-channel = int(arguments['--channel']) if arguments['--channel'] else 1
-pixel_size = float(arguments['--pixel-size']) if arguments['--pixel-size'] else 1
-rolling_ball_size = int(arguments['--rolling-ball-size']) if arguments['--rolling-ball-size'] else 30
+filter_window = arguments['--filter-window']
+gamma = arguments['--gamma']
+channel = arguments['--channel']
+pixel_size = arguments['--pixel-size'] if arguments['--pixel-size'] > 0 else None
+rolling_ball_size = arguments['--rolling-ball-size']
 
 keep_imgs = bool(arguments['--keep-imgs']) if arguments['--keep-imgs'] else False
 
