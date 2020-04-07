31166ccd283dddaa0f23cbd6514b60e614641266
Lucian DiPeso <ldipeso@uw.edu>
2020-04-07 16:28:59 -0700

    Bug fix in bash eq.

--- bash_functions.sh
+++ bash_functions.sh
@@ -1,8 +1,8 @@
 ner() {
   source "${HATCH_LAB_NE_RUPTURE_TOOL_PATH}/.venv/bin/activate"
-  if [ "${1}" -eq "segment" ]; then
+  if [ "${1}" == "segment" ]; then
     python "${HATCH_LAB_NE_RUPTURE_TOOL_PATH}/preprocess.py" aics "${@:2}"
-  elif [ "${1}" -eq "annotate" ]; then
+  elif [ "${1}" == "annotate" ]; then
     python "${HATCH_LAB_NE_RUPTURE_TOOL_PATH}/classify.py" manual "${@:2}"
   else
     echo "You may specify ner segment or ner annotate"
